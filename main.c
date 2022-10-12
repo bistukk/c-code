2a7d0bc30ccf3f9c51981f0af1f1c6403ed8c960
bistukk <114272925+bistukk@users.noreply.github.com>
2022-10-12 19:23:18 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=0;
+    int b=0;
+    for(a=0;a<100;a++)
+    {
+        printf("%d ",a);
+    }
+    printf("Hello world!\n");
+    return 0;
+}
